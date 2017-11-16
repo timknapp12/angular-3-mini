71077de5fb1c09999f0309360be1fdde2e9693c7
timknapp12 <timknapp12@gmail.com>
2017-11-16 16:18:59 -0700

    first commit

---
+++ app/cars/cars.component.js
@@ -0,0 +1,12 @@
+angular.module('fourWheels').component('cars', {
+    templateUrl: 'app/cars/cars.template.html',
+    controllerAs: 'carsCtrl',
+    controller: function(carsSrvc) {
+        this.cars = carsSrvc.cars;
+
+        this.buyCar = function(id) {
+            carsSrvc.buyCar(id);
+            this.cars = carsSrvc.cars;
+        }
+    }
+})
---
+++ app/footer/footer.component.js
@@ -0,0 +1,16 @@
+angular.module('fourWheels').component('footer', {
+    templateUrl: 'app/footer/footer.template.html',
+    controllerAs: 'footerCtrl',
+
+    controller: function() {
+        this.name = '';
+        this.email = '';
+        this.contacted = false;
+
+        this.contact = function() {
+            this.name = '';
+            this.email = '';
+            this.contacted = true;
+        }
+    }
+})
